b3fb046d6c4015eff31810b19b68906deb316d9f
hollowopk <1808479906@qq.com>
2021-09-21 20:51:38 +0800

    quicksort8x8

--- homework/Scene.java
+++ homework/Scene.java
@@ -23,7 +23,7 @@ public class Scene {
         Sorter bubbleSorter = new BubbleSorter();
         Sorter quickSorter = new QuickSorter();
 
-        theSnake.setSorter(bubbleSorter);
+        theSnake.setSorter(quickSorter);
 
         String log = theSnake.lineUp(line);
 
